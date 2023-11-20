5702758ff71d84091d6fe973ed3a77028d4a58b8
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-11-20 09:11:49 -0800

    quiz page finished

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 function Quiz(props) {
-  return <div>Quiz</div>;
+  // next button event handler
+  function onNext() {
+    console.log("Next question");
+  }
+
+  // previous button event handler
+  function onPrev() {
+    console.log("Prev question");
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application</h1>
+
+      {/* display questions */}
+
+      <div className="grid">
+        <button className="btn prev" onClick={onPrev}>
+          Prev
+        </button>
+        <button className="btn next" onClick={onNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
