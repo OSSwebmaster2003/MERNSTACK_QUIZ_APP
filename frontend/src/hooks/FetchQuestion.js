042b6535dc185e9cb823273b40925033fd28a5f1
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-11-20 21:49:45 -0800

    useFetchQuestions hook created

---
+++ frontend/src/hooks/FetchQuestion.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import data from "../database/data";
+// redux actions
+import * as Action from "../redux/question_reducer";
+
+// fetch question hook to fetch api data and set value to store
+
+export const useFetchQuestion = () => {
+  const dispatch = useDispatch();
+  const [getData, setGetData] = useState({
+    isLoading: false,
+    apiData: [],
+    serverError: null,
+  });
+
+  useEffect(() => {
+    setGetData((prev) => ({ ...prev, isLoading: true }));
+
+    // async function to get backend
+    (async () => {
+      try {
+        let questions = await data;
+
+        if (questions.length) {
+          setGetData((prev) => ({ ...prev, isLoading: false }));
+          setGetData((prev) => ({ ...prev, apiData: questions }));
+
+          // dispatch an action
+
+          dispatch(Action.startExamAction(questions));
+        } else {
+          throw new Error("No question available");
+        }
+      } catch (error) {
+        setGetData((prev) => ({ ...prev, isLoading: false }));
+        setGetData((prev) => ({ ...prev, serverError: error }));
+      }
+    })();
+  }, [dispatch]);
+
+  return [getData, setGetData];
+};
