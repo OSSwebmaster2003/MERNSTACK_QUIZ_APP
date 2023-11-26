fa6f7a9d1f8a451668dbd298d06317021cceec73
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-11-25 22:43:09 -0800

    backend server started

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { config } from "dotenv";
+
+const app = express();
+
+// app middleware
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+config();
+
+// application port
+const port = process.env.PORT || 8080;
+
+// express routes
+
+app.get("/", (req, res) => {
+  try {
+    res.json("Get Request");
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(port, (req, res) => {
+  console.log(`server connected to http://localhost:${port}`);
+});
