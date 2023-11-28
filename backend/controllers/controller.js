86874cd1db8bfddbab40620cf659c640b8b453ba
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-11-28 01:16:42 -0800

    questions controllers filled successfully

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,16 +1,36 @@
+import Questions from "../models/questionSchema.js";
+import Results from "../models/resultSchema.js";
+// import data
+import questions, { answers } from "../database/data.js";
+
 // get all questions
 export async function getQuestions(req, res) {
-  res.json("Questions api get request");
+  try {
+    const questions = await Questions.find();
+    res.json(questions);
+  } catch (error) {
+    res.json({ error });
+  }
 }
 
 // insert all questions
 export async function insertQuestions(req, res) {
-  res.json("Questions api post request");
+  try {
+    Questions.insertMany({ questions, answers });
+    res.json("Questions inserted successfully");
+  } catch (error) {
+    res.json({ error });
+  }
 }
 
 // delete all questions
 export async function deleteQuestions(req, res) {
-  res.json("Questions api delete request");
+  try {
+    await Questions.deleteMany();
+    res.json({ msg: "Questions deleted successfully" });
+  } catch (error) {
+    res.json({ error });
+  }
 }
 
 // get result
